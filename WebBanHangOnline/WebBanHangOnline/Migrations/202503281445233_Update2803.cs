25da725e043507d222d765350275baca4d570e94
VanTuNguyenUTT03 <nguyenvantuhandsome@gmail.com>
2025-04-03 09:06:55 +0700

    first commit

---
+++ WebBanHangOnline/WebBanHangOnline/Migrations/202503281445233_Update2803.cs
@@ -0,0 +1,36 @@
+﻿namespace WebBanHangOnline.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Update2803 : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.tb_ProductImage",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        ProductId = c.Int(nullable: false),
+                        Image = c.String(),
+                        IsDefault = c.Boolean(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.tb_Product", t => t.ProductId, cascadeDelete: true)
+                .Index(t => t.ProductId);
+            
+            CreateIndex("dbo.tb_OrderDetail", "ProductId");
+            AddForeignKey("dbo.tb_OrderDetail", "ProductId", "dbo.tb_Product", "Id", cascadeDelete: true);
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.tb_ProductImage", "ProductId", "dbo.tb_Product");
+            DropForeignKey("dbo.tb_OrderDetail", "ProductId", "dbo.tb_Product");
+            DropIndex("dbo.tb_ProductImage", new[] { "ProductId" });
+            DropIndex("dbo.tb_OrderDetail", new[] { "ProductId" });
+            DropTable("dbo.tb_ProductImage");
+        }
+    }
+}
